854d6ffbf84afbb73bdad97c029eb319d2d666ad
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-10-06 08:32:07 -0300

    Create inheritance.py

---
+++ app/agents/inheritance.py
@@ -0,0 +1,27 @@
+from bson import ObjectId
+from agents.crud import obter_agente
+
+def obter_agente_com_heranca(agente_id):
+    """Obtém um agente com os elementos herdados aplicados"""
+    agente = obter_agente(agente_id)
+    if not agente or not agente.get('agente_mae_id'):
+        return agente
+    
+    agente_mae = obter_agente(agente['agente_mae_id'])
+    if not agente_mae:
+        return agente
+    
+    elementos_herdar = agente.get('herdar_elementos', [])
+    agente_completo = agente.copy()
+    
+    for elemento in elementos_herdar:
+        if elemento == 'system_prompt' and not agente_completo.get('system_prompt'):
+            agente_completo['system_prompt'] = agente_mae.get('system_prompt', '')
+        elif elemento == 'base_conhecimento' and not agente_completo.get('base_conhecimento'):
+            agente_completo['base_conhecimento'] = agente_mae.get('base_conhecimento', '')
+        elif elemento == 'comments' and not agente_completo.get('comments'):
+            agente_completo['comments'] = agente_mae.get('comments', '')
+        elif elemento == 'planejamento' and not agente_completo.get('planejamento'):
+            agente_completo['planejamento'] = agente_mae.get('planejamento', '')
+    
+    return agente_completo
